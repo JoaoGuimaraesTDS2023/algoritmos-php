2aa986d3eff411d191051b64031d50553ce968ae
João G. S. Guimarães <134729473+JoaoGuimaraesTDS2023@users.noreply.github.com>
2023-11-24 13:49:26 -0300

    Update pokedex_terminal.php

--- atividades_sala/pokedex_terminal.php
+++ atividades_sala/pokedex_terminal.php
@@ -8,22 +8,42 @@
 
     );
 
-    $options = getopt($short, $long);
-
-    $nome = strtolower($options["pokemon"]);
-
-    $dados_texto = file_get_contents("https://pokeapi.co/api/v2/pokemon/$nome");
-
-    $pokemon = json_decode($dados_texto, true);
-
-    echo "Nome: " . strtoupper($pokemon['name']) . "\n";
-    echo "Altura: " . $pokemon['height']/10 . "m \n";
-    echo "Peso: " . $pokemon['weight']/10 . "kg \n";
-    echo "Movimentos: \n";
-
-    foreach($pokemon['moves'] as $movimentos)
+    if($argv[1] != NULL)
     {
-        echo "\t>" . $movimentos['move']['name'] . "\n";
+        $options = getopt($short, $long);
+        $nome = strtolower($options["pokemon"]);
+
+    }else
+    {
+        $nome = strtolower(readline("Nome do Pokemon: "));
+    }
+
+    pokedex($nome);
+
+    function pokedex(string $nome)
+    {
+        $dados_texto = file_get_contents("https://pokeapi.co/api/v2/pokemon/$nome");
+
+        $pokemon = json_decode($dados_texto, true);
+
+        $informacoes_pokemon = array(
+
+            "Nome" => strtoupper($pokemon['name']),
+            "Altura" => $pokemon['height'] / 10 . "m",
+            "Peso" => $pokemon['weight'] / 10 . "kg",
+        );
+
+        foreach($informacoes_pokemon as $key => $valor)
+        {
+            echo "$key: $valor \n";
+        }
+
+        echo "Movimentos: \n";
+
+        foreach($pokemon['moves'] as $movimentos)
+        {
+            echo "\t-" . $movimentos['move']['name'] . "\n";
+        }
     }
 
 ?>
